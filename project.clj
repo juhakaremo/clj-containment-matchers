9d69931c2a02e166875789ff1f7a0d4b20192db4
Juha Karemo <juha.karemo@reaktor.fi>
2014-03-19 09:17:55 +0200

    Add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-containment-matchers "0.9.0"
   :description "Matchers for clojure collections. This library is created especially to ease the pain of matching nested data structures."
-  :url ""
+  :url "https://github.com/juhakaremo/clj-containment-matchers"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src"]
   :test-paths ["test"])
