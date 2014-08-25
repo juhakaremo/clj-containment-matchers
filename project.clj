618053959301a59d25d7613022a33a741505e0ef
Juha Karemo <juha.karemo@reaktor.fi>
2014-08-25 08:34:18 +0300

    Update clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-containment-matchers "0.9.0"
+(defproject clj-containment-matchers "0.9.1"
   :description "Matchers for clojure collections. This library is created especially to ease the pain of matching nested data structures."
   :url "https://github.com/juhakaremo/clj-containment-matchers"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src"]
   :test-paths ["test"])
