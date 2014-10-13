8529129e86bcbb063ff186a7f0a008aa217ea9ed
Juha Karemo <juha.karemo@reaktor.fi>
2014-10-13 17:27:56 +0300

    Allow nil actual value

--- src/clj_containment_matchers/core.clj
+++ src/clj_containment_matchers/core.clj
@@ -12,6 +12,7 @@
 (def anything "clj-containment-matchers.core/anything-matcher")
 
 (defn diff+
+  "like clojure.data/diff but allows you to ignore values. Returns [things-only-in-expected things-only-in-actual things-in-both]"
   "like clojure.data/diff but allows you to ignore values"
   ([expected-raw actual-raw] (diff+ expected-raw actual-raw anything))
   ([expected-raw actual-raw keyword-value-to-ignore]
@@ -22,6 +23,13 @@
             [things-only-in-expected things-only-in-actual things-in-both] (diff expected actual)]
         [things-only-in-expected things-only-in-actual things-in-both])
       (throw (IllegalArgumentException. "diff+ accepts only collections. Consider using clojure.data/diff instead of diff+.")))))
+    (let [ignored-paths (get-anything-matchers keyword-value-to-ignore expected-raw)
+          expected (remove-paths expected-raw ignored-paths)]
+      (cond
+        (nil? actual-raw) [(remove-paths expected-raw ignored-paths) nil nil]
+        (and (coll? expected-raw) (coll? actual-raw)) (let [actual (remove-paths actual-raw ignored-paths)]
+                                                        (diff expected actual))
+        :else (throw (IllegalArgumentException. "diff+ accepts only collections. Consider using clojure.data/diff instead of diff+."))))))
 
 (defn contains-exactly?
   "Checks if expected and actual have exactly same.
