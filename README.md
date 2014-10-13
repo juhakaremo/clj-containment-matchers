d60178a3d87f290e0df6e1b4491a51c54e7690f4
Juha Karemo <juha.karemo@reaktor.fi>
2014-10-13 17:52:32 +0300

    Add license information

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Matchers for clojure collections. This library is created to ease the pain of matching nested data structures.
+
+#License
+
+The use and distribution terms for this software are covered by Eclipse LicensedPublic License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/juhakaremo/clj-containment-matchers"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :source-paths ["src"]
-  :test-paths ["test"])
+  :test-paths ["test"]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
