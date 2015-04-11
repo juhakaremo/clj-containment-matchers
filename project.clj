c1525412739114b11b0a5afac2187f4816b76fc3
Juha Karemo <juha.karemo@reaktor.fi>
2015-04-11 22:10:10 +0300

    Use ancient plugin

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
   :profiles {:dev {:dependencies [[org.clojure/tools.trace "0.7.8"]]}}
   :scm {:name "git"
         :url "https://github.com/juhakaremo/clj-containment-matchers"}
+  :plugins [[lein-ancient "0.6.6"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"})
