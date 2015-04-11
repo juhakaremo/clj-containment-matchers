5a0d7b58cb0d52a4d20480e3650e41a3c37e022c
Juha Karemo <juha.karemo@reaktor.fi>
2015-04-11 08:35:28 +0300

    Format source

--- test/clj_containment_matchers/core_test.clj
+++ test/clj_containment_matchers/core_test.clj
@@ -27,10 +27,12 @@
      (contains-exactly? {:name "john" :id "1"} {:name "john" :id anything})
      (does-not-contain-exactly? {:name "john" :id 1} {:name "john" :id string?}))
   (testing "compares nested maps"
-    (contains-exactly? {:name "john" :phone-numbers {"home" "123" "work" "456"}} {:name "john" :phone-numbers {"home" "123" "work" string?}})
-    (does-not-contain-exactly? {:name "john" :phone-numbers {"home" "123" "work" "456"}} {:name "john" :phone-numbers {"home" "123" "work-2" "456"}}))
+    (contains-exactly? {:name "john" :phone-numbers {"home" "123" "work" "456"}}
+                       {:name "john" :phone-numbers {"home" "123" "work" string?}})
+    (does-not-contain-exactly? {:name "john" :phone-numbers {"home" "123" "work" "456"}}
+                               {:name "john" :phone-numbers {"home" "123" "work-2" "456"}}))
   (testing "compares sets"
-    (contains-exactly? #{1 2 3} #{2 1 3}) ;no order, matchers functions are not supported either
+    (contains-exactly? #{1 2 3} #{2 1 3})
     (does-not-contain-exactly?  #{1 2 3} #{1 2 4}))
   (testing "compares nested structures with different types of collections"
     (contains-exactly?
