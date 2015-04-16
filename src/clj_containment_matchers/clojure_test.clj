d988df06652b285532539bbb86d5078d71eb542a
Markus Hjort <markus.hjort@reaktor.fi>
2015-04-16 15:21:32 +0300

    Extend clojure.test using multi method 'assert-expr'
    Instead of having separate macro 'contains-exactly?' which replaces the
    'is' macro we now extend clojure.test by adding two new assertions
    
    (is (equal? actual expected))
    AND
    (is (not-equal? actual expected))
    
    This is a de facto way that most of the other testing frameworks use for
    adding custom assertions and it is a recommended approach according to
    clojure.test documentation.
    
    Now 'with-junit-output' also works as expected. It was broken in version 1.0.0.
    
    Note! This does not provide as nice pretty printing yet than previous version did.

--- src/clj_containment_matchers/clojure_test.clj
+++ src/clj_containment_matchers/clojure_test.clj
@@ -1,37 +1,34 @@
 (ns clj-containment-matchers.clojure-test
   (:require [clj-containment-matchers.diff :refer [diff]]
             [clojure.pprint :refer [pprint]]
-            [clojure.test :refer [do-report]]))
+            [clojure.test :refer [do-report assert-expr]]))
 
 (defn anything [_] true)
 
-(defmethod clojure.test/report :fail-without-formatting [m]
-  (clojure.test/with-test-out
-    (clojure.test/inc-report-counter :fail)
-    (println "\nFAIL in" (clojure.test/testing-vars-str m))
-    (when (seq clojure.test/*testing-contexts*) (println (clojure.test/testing-contexts-str)))
-    (when-let [message (:message m)] (println message))
-    (println "expected:\n" (:expected m))
-    (println "actual:\n" (:actual m))))
-
-;This needs to be macro in order to have the correct line numbers in failure report
-(defmacro contains-exactly?
-  "Asserts that expected and actual have exactly same."
-  [actual expected]
- `(let [pretty# #(with-out-str (pprint %))
-        [things-only-in-actual# things-only-in-expected# things-in-both#] (diff ~actual ~expected)
+(defmethod assert-expr 'equal? [_ form]
+  "Asserts that expected and actual are exactly same. Prints a diff when not."
+  (let [actual (nth form 1)
+        expected (nth form 2)]
+ `(let [[things-only-in-actual# things-only-in-expected# things-in-both#] (diff ~actual ~expected)
         missing-expected-error-msg# (when things-only-in-expected#
-                                      (str "-- missing:\n" (pretty# things-only-in-expected#)))
+                                      (str "-- missing:\n" things-only-in-expected#))
         unexpected-error-msg# (when things-only-in-actual#
-                               (str "\n++ unexpected:\n" (pretty# things-only-in-actual#)))
+                               (str "\n++ unexpected:\n" things-only-in-actual#))
         error-msg# (str "\n" missing-expected-error-msg# unexpected-error-msg#)
-        success?# (and (empty? things-only-in-expected#) (empty? things-only-in-actual#))
-        [file# line#] (let [stacktrace# (.getStackTrace (RuntimeException.))
-                            ^StackTraceElement s# (first stacktrace#)]
-                        [(.getFileName s#)  (.getLineNumber s#)])]
-    (do-report {:type (if success?# :pass :fail-without-formatting)
+        success?# (and (empty? things-only-in-expected#) (empty? things-only-in-actual#))]
+    (do-report {:type (if success?# :pass :fail)
                 :message error-msg#
-                :expected (pretty# '~expected)
-                :actual (pretty# ~actual)
-                :file file#
-                :line line#})))
+                :expected '~expected
+                :actual ~actual}))))
+
+(defmethod assert-expr 'not-equal? [_ form]
+  "Asserts that expected and actual are not the same."
+  (let [actual (nth form 1)
+        expected (nth form 2)]
+ `(let [[things-only-in-actual# things-only-in-expected# things-in-both#] (diff ~actual ~expected)
+        success?# (not (and (empty? things-only-in-expected#)
+                            (empty? things-only-in-actual#)))]
+    (do-report {:type (if success?# :pass :fail)
+                :message "Should not be equal"
+                :expected '~expected
+                :actual ~actual}))))
